9ed5c33f5f53e73a3af67776dad1ddf737c05478
paulo_garcia <paulo.garcia@impossiblecode.ai>
2026-03-01 18:27:27 -0500

    [main] fix: update deployment paths and working directory for GitHub Pages

--- calculadora-pizza/vite.config.js
+++ calculadora-pizza/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/calculadora-pizza/',
+  base: '/pizza_dough_calculator/',
   plugins: [vue(), tailwindcss()],
 })
